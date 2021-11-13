344c602e01e345fa97d528cb146a5bf1d502a757
Ed <eberne@att.net>
2021-11-13 10:56:33 -0800

    Rewritten to fix flamethrown number of rolls.

--- comparison.py
+++ comparison.py
@@ -1,8 +1,8 @@
 from random import randrange
 from fractions import Fraction as frac
 
-n = 1000000  # one million (number to trials)
-faces = 6  # of dice
+n = 1000000  # one million (number of trials)
+faces = 6  # of faces on die
 
 def report():
     print(f'Monte Carlo result = {num_wounds / n:13.2%}')
@@ -12,34 +12,37 @@ def roll():
     return randrange(1, faces + 1)
 
 print('No rerolls')
+
 num_wounds = 0
-for i in range(n // 2):
+for i in range(n // 4):
     # flamethrower
-    if roll() >= 3: num_wounds += 1
+    for j in range(roll()):
+        if roll() >= 3: num_wounds += 1
     # melta
-    if roll() >= 4 and roll() >= 2: num_wounds += 1
+    if roll() >= 4 and roll() >= 2: num_wounds += 2
 # analytic = (frac(3, 6) + (frac(4 / 6) * frac(2 / 6))) / 2
-flame = frac(4, 6)
-melta = frac(5, 6) * frac(3, 6)
-analytic = (flame + melta) / 2
+flame = frac(4, 6) * frac(7, 2)  # 7/2 is the average result of rolling d6
+melta = frac(5, 6) * frac(3, 6) * 2  # melta has 2 men for every 1 flamethrower
+analytic = (flame + melta) / 4
 report()
 
 print('\nRerolls on hits')
 num_wounds = 0
-for i in range(n // 2):
+for i in range(n // 4):
     # flamethrower
-    if roll() >= 3: num_wounds += 1
+    for j in range(roll()):
+        if roll() >= 3: num_wounds += 1
     # melta
     first_reroll = True
     hits_roll = roll()
     if hits_roll >= 4 and roll() >= 2:
-        num_wounds += 1
+        num_wounds += 2
     elif hits_roll == 1 and first_reroll:
         first_reroll = False
-        if roll() >= 2: num_wounds += 1
+        if roll() >= 4 and roll() > 2: num_wounds += 1
     # hits_reroll happens when hits roll = 1, i.e. 1/6 of the time
     # then it succeeds when roll() >= 2, i.e. 5/6 of the time
-hits_reroll = frac(1, 6) * frac(5, 6)
+hits_reroll = frac(1, 6) * frac(5, 6) * 2  # 1/6 chance of reroll then regular
 analytic += (hits_reroll / 2)  # only applies to melta, i.e. half the time
 report()
 
---
+++ warhammer_2.py
@@ -0,0 +1,71 @@
+from random import randrange
+from fractions import Fraction as frac
+
+num_rounds = 100000  # one million
+
+def report():
+    print(f'Monte Carlo result = {(total_score / 10) / num_rounds:14.2%}')
+    print(f'Analytic result = {str(analytic):>7} or {(analytic.numerator / analytic.denominator):.2%}')
+
+def roll():
+    return randrange(1, 7)
+
+print("Base Case")
+total_score = 0
+for i in range(num_rounds):
+    # flamethrower
+    for j in range(roll()):
+        if roll() >= 3:
+            total_score += 1
+
+    # meltagun
+    hit_roll = roll()
+    if hit_roll >= 4 and roll() >= 2:
+        total_score += 4
+
+''' Analysis of probability
+Flamethrowers number of rolls = 7/2 which is average roll of six sided dice
+    on each roll they score 3+ which has a probability of 4/6
+Meltaguns get 4 rolls per turn
+    to score they must roll 4+, probability of 3/6 and 2+, probability of 5/6
+'''
+analytic = (frac(4, 6) * frac(7, 2) + frac(5, 6) * frac(3, 6) * 4) / 10
+report()
+
+print("\nReroll on hit")
+total_score = 0
+for i in range(num_rounds):
+    # flamethrower - same as base case
+    for j in range(roll()):
+        if roll() >= 3: total_score += 1
+
+    # meltagun - do reroll if hit_roll == 1
+    hit_roll = roll()
+    if hit_roll >= 4:
+        if roll() >= 2: total_score += 4
+    elif hit_roll == 1:  # reroll
+        if roll() >= 4 and roll() >= 2: total_score += 4
+
+analytic += (frac(1, 6) * frac(5, 6) * frac(3, 6) * 4) / 10
+report()
+
+print("\nReroll on wound")
+total_score = 0
+for i in range(num_rounds):
+    # flamethrower
+    for j in range(roll()):
+        wound_roll = roll()
+        if wound_roll >= 3:
+            total_score += 1
+        elif wound_roll == 1:
+            if roll() >= 3: total_score += 1
+
+    # meltagun
+    wound_roll = roll()
+    if roll() >= 4 and wound_roll > 2:
+        total_score += 4
+    elif wound_roll == 1:
+        if roll() >= 4 and roll() >= 2: total_score += 4
+
+analytic += (frac(1, 6) * frac(3, 6)) / 10
+report()
