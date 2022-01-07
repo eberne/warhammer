f615956ec046458871f8bd26098028cb088ea6e7
Ed <eberne@att.net>
2022-01-07 12:53:21 -0800

    Extended Weapon class to account for non-constant damage

--- class_test.py
+++ class_test.py
@@ -1,17 +1,26 @@
+# import random
+import random
+
 class Unit:
     def __init__(self, name, num_people, person_list):
         self.name = name
         self.num_people = num_people
         self.person_list = person_list
 
-
 class Person:
     def __init__(self, name, weapon_list):
         self.name = name
         self.weapon_list = weapon_list
 
-
 class Weapon:
-    def __init__(self, damage, num_shots):
+    def __init__(self, name, damage, num_shots):
+        self.name = name
         self.damage = damage
         self.num_shots = num_shots
+
+    @classmethod
+    def damage_chooser(cls, damage):
+        if damage == 'D6':
+            return random.randint(1, 6)
+        # insert other non-constant damage rules here if necessary
+        return damage
